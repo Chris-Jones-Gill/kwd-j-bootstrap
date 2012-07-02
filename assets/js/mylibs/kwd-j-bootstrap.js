0442e5114935b5d9d0d4d9064475fcf8a21c3acf
Chris Jones-Gill <chris.jones-gill@kisswebdesign.co.uk>
2012-07-02 14:48:16 +0100

    Tidied up my the js, made it pass jslint. Did the same while reading through application.js to understand it better.

--- assets/js/mylibs/kwd-j-bootstrap.js
+++ assets/js/mylibs/kwd-j-bootstrap.js
@@ -2,7 +2,7 @@
  * Template:	kwd-j-bootstrap :: for Joomla! 2.5
  * File:		kwd-j-bootstrap.js
  * Author: 		Chris Jones-Gill - KISS Web Design
- * Version: 	0.0.3
+ * Version: 	0.0.4
  * Created: 	March 2012
  * Copyright:	KISS Web Design - (C) 2012 - All rights reserved
  * License:		Apache Version 2.0 - http://www.apache.org/licenses/LICENSE-2.0
@@ -11,41 +11,42 @@
 /* ===================================================================== */
 
 $.noConflict();
-!function ($) {
+(function kwdjb($) {
+    "use strict";
 // collapse, accordion style, each active content row when viewed on small screens
 // requires bootstrap-collapse.js and bootstrap-transition.js loaded before this file
 
-function rowSwitch(width, rownum) {
+    function rowSwitch(width, rownum) {
 
-var row = jQuery('#collapserow'+rownum);
-var rowheading = jQuery('#row'+rownum+'_collapse_text');
+        var row = jQuery('#collapserow' + rownum),
+            rowheading = jQuery('#row' + rownum + '_collapse_text');
 
-width = jQuery(window).width();
-if (width < 768){
-	row.removeClass('in');
-	rowheading.addClass('heading_show');
-	}
+        width = jQuery(window).width();
+        if (width < 768) {
+            row.removeClass('in');
+            rowheading.addClass('heading_show');
+        }
 
-row.collapse({
-    toggle: false
-    })
+        row.collapse({
+            toggle: false
+        });
 
-$('#collapserow'+rownum).on('shown', function () {
-		rowheading.removeClass('heading_show');
-		rowheading.addClass('heading_hide');		
-    })
-$('#collapserow'+rownum).on('hidden', function () {
-		rowheading.addClass('heading_show');
-		rowheading.removeClass('heading_hide');
-    })
-}
-rowSwitch(0,2);
-rowSwitch(0,4);
-rowSwitch(0,6);
-rowSwitch(0,7);
-rowSwitch(0,10);
-rowSwitch(0,11);
-rowSwitch(0,12);
-rowSwitch(0,13);
+        $('#collapserow' + rownum).on('shown', function () {
+            rowheading.removeClass('heading_show');
+            rowheading.addClass('heading_hide');
+        });
+        $('#collapserow' + rownum).on('hidden', function () {
+            rowheading.addClass('heading_show');
+            rowheading.removeClass('heading_hide');
+        });
+    }
+    rowSwitch(0, 2);
+    rowSwitch(0, 4);
+    rowSwitch(0, 6);
+    rowSwitch(0, 7);
+    rowSwitch(0, 10);
+    rowSwitch(0, 11);
+    rowSwitch(0, 12);
+    rowSwitch(0, 13);
 
-}(window.jQuery)
+}(window.jQuery));
